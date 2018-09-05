3c29d0ab92000b8371760e75dceb6330e1978f4a
Omid Vatandoost <omidvatan94@gmail.com>
2018-09-05 00:48:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
 // add solution here
-function theBeatlesPlay(artists, instruments) {
-  var allArtists = [];
-  for(i=0; i < players.length; i++) {
-    allArtists.push(players[i] + " plays" + instruments[i]);
-  }
-  return allArtists;
-}
