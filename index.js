2b223b3f0fd690eacba8a61005cba3e388700a43
Omid Vatandoost <omidvatan94@gmail.com>
2018-09-05 01:08:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ function theBeatlesPlay(players, instruments) {
     }
     return allPlayers;
 }
-function johnLennonFacts(facts) {
-    var facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
+function johnLennonFacts() {
+    const facts = ["He was the last Beatle to learn to drive", "He was never a vegetarian", "He was a choir boy and boy scout", "He hated the sound of his own voice"];
     var newFacts = [];
     var i = 0;
     while (i < facts.length) {
