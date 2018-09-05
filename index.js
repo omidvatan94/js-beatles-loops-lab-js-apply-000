68763c4ac1742084ca04c09dd8ee673621fdfae8
Omid Vatandoost <omidvatan94@gmail.com>
2018-09-05 00:48:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // add solution here
 function theBeatlesPlay(artists, instruments) {
-  var allArtists = []
+  var allArtists = [];
   for(i=0; i < players.length; i++) {
     allArtists.push(players[i] + " plays" + instruments[i]);
   }
