e66e282aa5bf8aef7d6f199df4a3261b15fba4b6
Omid Vatandoost <omidvatan94@gmail.com>
2018-09-05 00:45:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 function theBeatlesPlay(artists, instruments) {
   var allArtists = []
   for(i=0; i < players.length; i++) {
-    allArtists.push(players[i] + "")
+    allArtists.push(players[i] + " plays" + )
   }
 }
